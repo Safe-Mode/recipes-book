a8dc9f570325777ef4e066626f53c7376d56dbcb
Safe_Mode <safe2mode@gmail.com>
2020-01-06 21:06:28 +0300

    Implement add ingredients to shopping list by custom events

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('titleInput', { static: true }) titleInput: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddIngredientBtnClick() {
+    const newIngredient = new Ingredient(
+      this.titleInput.nativeElement.value,
+      this.amountInput.nativeElement.value
+    );
+
+    this.ingredientAdded.emit(newIngredient);
+    this.titleInput.nativeElement.value = '';
+    this.amountInput.nativeElement.value = '';
+  }
 }
