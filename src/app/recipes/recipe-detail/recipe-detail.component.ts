0a23932d570e33099b5f79b9d82c120b23c9358f
Safe_Mode <safe2mode@gmail.com>
2020-01-19 21:55:39 +0300

    Implement recipe ingredients add to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../models/recipe.model';
+import { ShoppingListService } from 'src/app/services/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +10,15 @@ import { Recipe } from '../../models/recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(
+    private shoppingListService: ShoppingListService
+  ) { }
 
   ngOnInit() {
   }
 
+  onToShoppingListClick(evt: Event) {
+    evt.preventDefault();
+    this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from '../models/recipe.model';
+import { Ingredient } from '../models/ingredient.model';
 
 
 export class RecipeService {
@@ -8,14 +9,34 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe 1',
-      'Simple test description',
-      'https://cdn.apartmenttherapy.info/image/upload/v1564775676/k/Photo/Recipes/2019-08-how-to-juiciest-turkey-meatballs/How-to-Make-the-Best-Juiciest-Turkey-Meatballs_055.jpg'
+      'Touchdown Chili',
+      'Every year when football season rolls around and the weather begins to cool, my husband requests I make this hearty, beanless chili. It receives rave reviews from everyone.',
+      'https://images.media-allrecipes.com/userphotos/600x600/5583115.jpg',
+      [
+        new Ingredient('2 pounds ground beef', 2),
+        new Ingredient('1 large onion, chopped', 1),
+        new Ingredient('6 cloves garlic, chopped', 6),
+        new Ingredient('1/3 cup chili powder', 0),
+        new Ingredient('1 1/2 teaspoons ground cumin', 1),
+        new Ingredient('1 1/2 teaspoons dried basil', 1),
+        new Ingredient('1 (28 ounce) can diced tomatoes with juice', 1),
+        new Ingredient('1 (4 ounce) can diced green chile peppers, drained', 1)
+      ]
     ),
     new Recipe(
-      'Test Recipe 2',
-      'Simple test description',
-      'https://cdn.apartmenttherapy.info/image/upload/v1564775676/k/Photo/Recipes/2019-08-how-to-juiciest-turkey-meatballs/How-to-Make-the-Best-Juiciest-Turkey-Meatballs_055.jpg'
+      'Restaurant-Style Buffalo Chicken Wings',
+      'This is similar to the hot wings recipe served at a popular restaurant chain. If you have ever had them, you have to love them.',
+      'https://images.media-allrecipes.com/userphotos/4614563.jpg',
+      [
+        new Ingredient('½ cup all-purpose flour', 0),
+        new Ingredient('¼ teaspoon paprika', 0),
+        new Ingredient('¼ teaspoon cayenne pepper', 0),
+        new Ingredient('¼ teaspoon salt', 0),
+        new Ingredient('10 chicken wings', 10),
+        new Ingredient('oil for deep frying', 1),
+        new Ingredient('¼ cup butter', 0),
+        new Ingredient('¼ cup hot sauce', 0)
+      ]
     )
   ];
 
