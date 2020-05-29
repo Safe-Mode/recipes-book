f2c01e91bf41e170a0d100e8dd6c9a73ff8dcdf9
Safe_Mode <safe2mode@gmail.com>
2020-05-29 16:13:56 +0300

    Init firebase hosting config

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { ShortenPipe } from './shorten.pipe';
 import { DropdownDirective } from './directives/dropdown.directive';
 import { DynamicCmpHostDirective } from './directives/dynamic-cmp-host.directive';
 import { EqualPasswordValidatorDirective } from './directives/equal-password.directive';
-import { ShortenPipe } from './shorten.pipe';
 import { LogoComponent } from './components/logo/logo.component';
 import { ModalComponent } from './components/modal/modal.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
